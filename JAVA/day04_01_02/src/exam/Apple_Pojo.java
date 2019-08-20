1d1449174b51aba5208d033eb43f42816d3a5087
kyusikjeong <ksjeong860@gmail.com>
2019-08-20 21:36:04 +0900

    JAVA_WebApp/NetBeans
    간단한 메소드 제작, 접근지정자, POJO 제작.
    pojo:순수한 값만 가진 클래스. setget

---
+++ JAVA/day04_01_02/src/exam/Apple_Pojo.java
@@ -0,0 +1,38 @@
+package exam;
+
+public class Apple_Pojo {//setter  getter클래스. 입력받은 데이터들을 분류, 리턴
+	
+	private String type;
+	private String origine;
+	private int status;
+	private int size;
+	
+	
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getOrigine() {
+		return origine;
+	}
+	public void setOrigine(String origine) {
+		this.origine = origine;
+	}
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+	
+	
+}
